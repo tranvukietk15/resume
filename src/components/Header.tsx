371d95ca69e843bea7545d17998f0ec1ed7ad5ef
kiettran <tranvukietk15@gmail.com>
2021-06-01 00:35:51 +0700

    update skills

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,7 @@ const Header = () => {
         <ul>
           <li><a href="#hero" className="nav-link scrollto active"><i className="bx bx-home" /> <span>Home</span></a></li>
           <li><a href="#about" className="nav-link scrollto"><i className="bx bx-user" /> <span>About</span></a></li>
+          <li><a href="#skills" className="nav-link scrollto"><i className="bx bx-analyse" /> <span>Skills</span></a></li>
           <li><a href="#resume" className="nav-link scrollto"><i className="bx bx-file-blank" /> <span>Resume</span></a></li>
           <li><a href="#portfolio" className="nav-link scrollto"><i className="bx bx-book-content" /> <span>Portfolio</span></a></li>
           {/* <li><a href="#services" className="nav-link scrollto"><i className="bx bx-server" /> <span>Services</span></a></li> */}
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Skills = () => {
   return (
     <section id="skills" className="skills section-bg">
-      <div className="container" data-aos="fade-up">
+      <div className="container resume" data-aos="fade-up">
         <div className="section-title">
           <h2>Skills</h2>
         </div>
@@ -11,196 +11,195 @@ const Skills = () => {
 
           <div className="col-lg-4 col-4">
             <div className="progress">
-              <span className="skill">Programming languages<i className="val"></i></span>
+              <span className="text-center color-bl"><h5>Programming languages</h5><i className="val"></i></span>
             </div>
             <div className="progress">
               <span className="skill">C#<i className="val">2-5 years</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={75} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={75} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
             <div className="progress">
               <span className="skill">Javascript<i className="val">1-2 years</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
           </div>
 
           <div className="col-lg-4 col-4">
             <div className="progress">
-              <span className="skill">Frameworks &amp; Libraries<i className="val"></i></span>
+            <span className="text-center color-bl"><h5>Frameworks &amp; Libraries</h5><i className="val"></i></span>
             </div>
             <div className="progress">
               <span className="skill">.Net core<i className="val">2-5 years</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
             <div className="progress">
               <span className="skill">ReactJS<i className="val">1-2 years</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
             <div className="progress">
               <span className="skill">Angular 2+<i className="val">1-6 months</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
           </div>
 
           <div className="col-lg-4 col-4">
             <div className="progress">
-              <span className="skill">Data Access<i className="val"></i></span>
+              <span className="text-center color-bl"><h5>Data Access</h5><i className="val"></i></span>
             </div>
             <div className="progress">
               <span className="skill">Entity Framework<i className="val">2-5 years</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
           </div>
 
           <div className="col-lg-4 col-4">
             <div className="progress">
-              <span className="skill">Database <i className="val"></i></span>
+              <span className="text-center color-bl"><h5>Database</h5><i className="val"></i></span>
             </div>
             <div className="progress">
               <span className="skill">Amazon DynamoDB<i className="val">1-2 years</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
             <div className="progress">
               <span className="skill">MSSQL<i className="val">6-12 months</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
             <div className="progress">
               <span className="skill">MySQL<i className="val">1-2 years</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
           </div>
 
           <div className="col-lg-4 col-4">
             <div className="progress">
-              <span className="skill">Unit Testing and Mocking Frameworks<i className="val"></i></span>
+              <span className="text-center color-bl"><h5>Unit Testing and Mocking Frameworks</h5><i className="val"></i></span>
             </div>
             <div className="progress">
               <span className="skill">Moq<i className="val">1-2 years</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
             <div className="progress">
               <span className="skill">Nunit<i className="val">1-2 years</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
           </div>
 
           <div className="col-lg-4 col-4">
             <div className="progress">
-              <span className="skill">General Development Skills<i className="val"></i></span>
+              <span className="text-center color-bl"><h5>General Development Skills</h5><i className="val"></i></span>
             </div>
             <div className="progress">
               <span className="skill">Object-Oriented Programming (OOP)<i className="val">2-5 years</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
             <div className="progress">
               <span className="skill">Unit Testing<i className="val">1-2 years</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
             <div className="progress">
               <span className="skill">SOLID Principle<i className="val">1-2 years</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
             <div className="progress">
               <span className="skill">Clean Code and Coding refactoring<i className="val">1-2 years</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
             <div className="progress">
               <span className="skill">Dependency Injection<i className="val">1-2 years</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
           </div>
 
           <div className="col-lg-4 col-4">
             <div className="progress">
-              <span className="skill">Design Pattern<i className="val"></i></span>
+              <span className="text-center color-bl"><h5>Design Pattern</h5><i className="val"></i></span>
             </div>
             <div className="progress">
               <span className="skill">Common Design Patterns<i className="val">1-2 years</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
           </div>
 
           <div className="col-lg-4 col-4">
             <div className="progress">
-              <span className="skill">DevOps<i className="val"></i></span>
+              <span className="text-center color-bl"><h5>DevOps</h5><i className="val"></i></span>
             </div>
             <div className="progress">
               <span className="skill">Docker<i className="val">1-6 months</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
           </div>
 
           <div className="col-lg-4 col-4">
             <div className="progress">
-              <span className="skill">AWS<i className="val"></i></span>
+              <span className="text-center color-bl"><h5>AWS</h5><i className="val"></i></span>
             </div>
             <div className="progress">
               <span className="skill">Amazon Simple Queue Service<i className="val">6-12 months</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
             <div className="progress">
               <span className="skill">Amazon Simple Notification Service<i className="val">6-12 months</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
             <div className="progress">
               <span className="skill">Amazon S3<i className="val">6-12 months</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
             <div className="progress">
               <span className="skill">Amazon CloudWatch<i className="val">6-12 months</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
             <div className="progress">
               <span className="skill">Amazon CloudFront<i className="val">6-12 months</i></span>
               <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
+                <div className="progress-bar w-100" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </section>
